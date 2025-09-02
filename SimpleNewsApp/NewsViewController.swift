e9cc849edf5de7e0d4a4f1dc08c18f06fe55e548
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2025-09-02 10:31:43 +0200

    problem with packages fixed

---
+++ SimpleNewsApp/NewsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  SimpleNewsApp
+//
+//  Created by Lora Zubic on 02.09.2025..
+//
+
+import UIKit
+import PureLayout
+
+class NewsViewController: UIViewController {
+    private let newsTableView = UITableView()
+    private let mockData = ["news1", "news2", "news3", "news4", "news5", "news6", "news7", "news8", "news9", "news10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        addSubviews()
+        setupTableView()
+        editViews()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(newsTableView)
+        
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        newsTableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "NewsTableViewCell")
+        
+        newsTableView.autoPinEdgesToSuperviewMargins()
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func editViews() {
+        
+    }
+    
+    
+}
+
+extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        mockData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let new = mockData[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath) as? NewsTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
--- SimpleNewsApp/SceneDelegate.swift
+++ SimpleNewsApp/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let vc = ViewController()
+        let vc = NewsViewController()
         let nvc = UINavigationController(rootViewController: vc)
         let window: UIWindow = .init(windowScene: windowScene)
         window.rootViewController = nvc
